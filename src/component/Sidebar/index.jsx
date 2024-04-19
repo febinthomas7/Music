b378fa946aaa2329eea958fcd7c11e0db4c2f8e1
febin thomas <tfebin39@gmail.com>
2024-04-19 21:13:53 +0530

    scroll elem fixed

--- src/component/Sidebar/index.jsx
+++ src/component/Sidebar/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="w-[0px] md:w-[20%]  bg-black text-white fixed">
+    <div className="w-[0px] md:w-[20%]  h-svh bg-black text-white fixed">
       <div className="flex items-center justify-center gap-2 p-4">
         {/* <img src="/logo.png" alt="logo" className="w-[40px] sm:w-[60px]" />
         <h1 className="text-[30px] md:text-[30px] hidden sm:flex">GanaBajao</h1> */}
